3f84050361a6c597d4d51beb1dfc8b23aed79fd0
Likhit Garimella <likhitgmella@gmail.com>
2020-11-08 18:09:23 +0530

    3 buttons & 1st cont view

--- HackBattle/HomeViewController.swift
+++ HackBattle/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  HackBattle
 //
 //  Created by Likhit Garimella on 29/10/20.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
