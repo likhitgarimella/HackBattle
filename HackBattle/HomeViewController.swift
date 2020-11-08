1155171c7b8208eb96bbc950acf6224414374e12
Likhit Garimella <likhitgmella@gmail.com>
2020-11-08 18:41:57 +0530

    default props set

--- HackBattle/HomeViewController.swift
+++ HackBattle/HomeViewController.swift
@@ -18,12 +18,26 @@ class HomeViewController: UIViewController {
     @IBOutlet var registerView: UIView!
     @IBOutlet var tracksView: UIView!
     @IBOutlet var prizesView: UIView!
+    
+    func Default() {
+        
+        /// Dark blue color theme
+        register.setTitleColor(UIColor(red: 15/255, green: 60/255, blue: 122/255, alpha: 1.0), for: .normal)
+        /// Gray color theme
+        tracks.setTitleColor(UIColor(red: 110/255, green: 120/255, blue: 130/255, alpha: 1.0), for: .normal)
+        /// Gray color theme
+        prizes.setTitleColor(UIColor(red: 110/255, green: 120/255, blue: 130/255, alpha: 1.0), for: .normal)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // remove title for left bar button item
+        navigationController?.navigationBar.topItem?.title = ""
         
+        Default()
         
     }
     
-}   // #30
+}   // #44
