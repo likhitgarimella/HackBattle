5a1e9b14b15f78c401f8a7d415133e376dd15ab8
Likhit Garimella <likhitgmella@gmail.com>
2020-11-11 15:06:32 +0530

    tracks screens 1 card done with all props

--- HackBattle/TracksViewController.swift
+++ HackBattle/TracksViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class TracksViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var viewOne: UIView!
+    
+    func Properties() {
+        
+        // MARK: - Corner radius
+        viewOne.layer.cornerRadius = 15
+        
+        // MARK: - Shadow properties
+        viewOne.layer.shadowColor = UIColor(red: 15/255, green: 60/255, blue: 122/255, alpha: 1.0).cgColor
+        viewOne.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        viewOne.layer.shadowRadius = 6.0
+        viewOne.layer.shadowOpacity = 0.3
+        viewOne.layer.masksToBounds = false
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        Properties()
         
     }
     
