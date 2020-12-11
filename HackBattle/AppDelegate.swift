3a624d59e55b130ccda4ca156499b53f14460739
Likhit Garimella <likhitgmella@gmail.com>
2020-12-11 16:38:50 +0530

    min dep target change from 11.0 to 13.0 for the sake of onboarding func; proj settings dep target, app delg code, scene delg code

--- HackBattle/AppDelegate.swift
+++ HackBattle/AppDelegate.swift
@@ -36,24 +36,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
 
-    // MARK: UISceneSession Lifecycle
-    @available(iOS 13.0, *)
+    // MARK:- UISceneSession Lifecycle
+
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        if #available(iOS 13.0, *) {
-            return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-        } else {
-            // Fallback on earlier versions
-        }
-        return UISceneConfiguration(name: "False", sessionRole: connectingSceneSession.role)
-        }
-    
-    @available(iOS 13.0, *)
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
-}   // #60
+}   // #54
