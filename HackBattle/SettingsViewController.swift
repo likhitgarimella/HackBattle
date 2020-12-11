d86aeee7c1d44d308c8ec9ecb4e7c9c389d64879
Likhit Garimella <likhitgmella@gmail.com>
2020-12-11 13:32:23 +0530

    settings VC code deleted

--- HackBattle/SettingsViewController.swift
+++ HackBattle/SettingsViewController.swift
@@ -8,50 +8,12 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-    
-    // Outlets
-    @IBOutlet var modeSwitch: UISwitch!
-    
-    let appMode = Mode()
-    
-    /*
-    func SwitchToDarkMode() {
-        view.backgroundColor = .black
-        self.tabBarController?.tabBar.backgroundColor = .black
-        UITabBar.appearance().barTintColor = .black
-    }
-    
-    func SwitchToLightMode() {
-        view.backgroundColor = .white
-        self.tabBarController?.tabBar.backgroundColor = .white
-        UITabBar.appearance().barTintColor = .white
-    }
-    */
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        appMode.mode = false
-        // print(appMode.mode)
+        
         
     }
     
-    @IBAction func switchMode(_ sender: UISwitch) {
-        
-        /*
-        if modeSwitch.isOn == true {
-            appMode.mode = true
-            // print(appMode.mode)
-            SwitchToDarkMode()
-        }
-        
-        if modeSwitch.isOn == false {
-            appMode.mode = false
-            // print(appMode.mode)
-            SwitchToLightMode()
-        }
-        */
-        
-    }
-    
-}   // #58
+}   // #20
