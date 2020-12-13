76da20b2f00510f0a77da730a1e446932cdb606b
Likhit Garimella <likhitgmella@gmail.com>
2020-12-13 19:10:54 +0530

    faq screen table view, sl no image, content view setup

---
+++ HackBattle/FaqTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  FaqTableViewCell.swift
+//  HackBattle
+//
+//  Created by Likhit Garimella on 13/12/20.
+//
+
+import UIKit
+
+class FaqTableViewCell: UITableViewCell {
+    
+    // Outlets
+    @IBOutlet var serialNumber: UIImageView!
+    @IBOutlet var faqView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+        
+    }
+    
+}   // #31
--- HackBattle/FaqsViewController.swift
+++ HackBattle/FaqsViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class FaqsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class FaqsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Outlets
+    @IBOutlet weak var faqTableView: UITableView!
+    
+    var items1 = [UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6")]
+    
+    func Properties() {
         
-        
+        faqTableView.backgroundColor = UIColor.white
         
     }
     
-}   // #20
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Properties()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items1.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell1 = tableView.dequeueReusableCell(withIdentifier: "FaqTableViewCell", for: indexPath as IndexPath) as! FaqTableViewCell
+        cell1.backgroundColor = UIColor.white
+        cell1.serialNumber.image = items1[indexPath.row]
+        return cell1
+    }
+    
+}   // #42
